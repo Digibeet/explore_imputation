0d45980b8d3fcf1a06bd4746c96b16d0099edb5b
Rianne Schouten <rianne.schouten@dpa.nl>
2017-10-23 16:14:02 +0200

    added confidence intervals

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,3 @@
-lines = []
-
 jQuery.get('data/results.txt', function(data) {
     lines = data.split("\n");
     var header = lines[0];
@@ -14,11 +12,18 @@ jQuery.get('data/results.txt', function(data) {
 
     data = {"MCAR":[], "MAR":[], "MNAR":[]};
 
-    desired_columns = ["mean", "median","drop"];
+    desired_columns = ["drop", "mean"]; // This needs to be interactive (start with just drop en mean)
     desired_columns_indexes = []
-    for (column_index in desired_columns){
-        column = desired_columns[column_index]
-        desired_columns_indexes.push(header_map[column])
+    confidence_interval = true; // This needs to be interactive (yes/no)
+
+    for (index in desired_columns){
+        column = desired_columns[index]
+        column_index = header_map[column]
+        desired_columns_indexes.push(column_index)
+        if (confidence_interval){    
+        desired_columns_indexes.push(parseInt(column_index) + parseInt(1)) //lower bound
+        desired_columns_indexes.push(parseInt(column_index) + parseInt(2)) //upper bound
+        }
     }
 
     x_column_index = header_map["missing_perc"];
@@ -33,8 +38,10 @@ jQuery.get('data/results.txt', function(data) {
         var missing_type = fields[missing_type_index]
 
         var desired_fields = {"x":fields[x_column_index]}
+
         for (index in desired_columns_indexes){
-            desired_fields[desired_columns[index]] = fields[desired_columns_indexes[index]]
+            column_name = columns[desired_columns_indexes[index]]
+            desired_fields[column_name] = fields[desired_columns_indexes[index]]
         }
 
         if(data[missing_type]) {
@@ -47,16 +54,14 @@ jQuery.get('data/results.txt', function(data) {
 
     series = [];
 
-    missing_type = "MCAR"
+    desired_missing_type = "MCAR" // This needs to be interactive
 
     for (imputation_method_index in desired_columns) {
         imputation_method = desired_columns[imputation_method_index]
-        
-        var line_data = data[missing_type].map(function(point, index) {
+     
+        var line_data = data[desired_missing_type].map(function(point, index) {
           return [parseFloat(point["x"]), parseFloat(point[imputation_method])];
         });
-
-        console.log(line_data)
 
         line = {
             name: imputation_method,
@@ -68,15 +73,39 @@ jQuery.get('data/results.txt', function(data) {
                 lineColor: Highcharts.getOptions().colors[0]
             }
         }
-        series.push(line)
-    }
 
-    console.log(data["MCAR"])
+        series.push(line)
+
+        if (confidence_interval){
+            lower_bound_imputation_method = columns[parseInt(header_map[imputation_method]) + parseInt(1)]
+            upper_bound_imputation_method = columns[parseInt(header_map[imputation_method]) + parseInt(2)]
+
+            var range_data = data[desired_missing_type].map(function(point, index) {
+                return [parseFloat(point["x"]), parseFloat(point[lower_bound_imputation_method]), parseFloat(point[upper_bound_imputation_method])];
+            });
+
+            range = {
+                name: 'ci_range',
+                data: range_data,
+                type: 'arearange',
+                lineWidth: 0,
+                linkedTo: ':previous',
+                color: Highcharts.getOptions().colors[0],
+                fillOpacity: 0.3,
+                zIndex: 0,
+                marker: {
+                    enabled: false
+                }
+            }
+
+            series.push(range)
+        }
+    }
 
     Highcharts.chart('container', {
 
     title: {
-        text: 'July temperatures'
+        text: 'Missing Data Simulation Output'
     },
 
     xAxis: {
