44249fc98d6859a8fd7b4316c12d119405cbf493
Rianne Schouten <rianne.schouten@dpa.nl>
2017-10-24 12:49:38 +0200

    working plot and structured code

--- js/charts.js
+++ js/charts.js
@@ -3,69 +3,121 @@ jQuery.get('data/results.txt', function(data) {
     lines = data.split("\n");
     var header = lines[0];
     lines.shift()
-    columns = header.split("\t");
+    column_names = header.split("\t");
 
     header_map = {};
-    columns.forEach(function (column, index) {
+    column_names.forEach(function (column, index) {
         header_map[column] = index;
     });
 
-    data = {"MCAR":[], "MAR":[], "MNAR":[]};
+    // Default settings
+    x_variable = 'missing_perc'
+    missing_type = 'MAR'
+    evaluation_error_metric = 'RMSE'
+    evaluation_model = 'lin'
+    desired_columns = ['drop', 'median']
+    confidence_interval = true
 
-    desired_columns = ["drop", "mean"]; // This needs to be interactive (start with just drop en mean, options: also median)
+    x_variable_index = header_map[x_variable];
+    missing_type_index = header_map['missing_type'];
+    evaluation_model_index = header_map['model']
+
     desired_columns_indexes = []
-    confidence_interval = true; // This needs to be interactive (true/false)
-
     for (index in desired_columns){
         column = desired_columns[index]
         column_index = header_map[column]
         desired_columns_indexes.push(column_index)
 
-        if (confidence_interval){    
+        if (confidence_interval) {    
         desired_columns_indexes.push(parseInt(column_index) + parseInt(1)) //lower bound
         desired_columns_indexes.push(parseInt(column_index) + parseInt(2)) //upper bound
         }
     }
 
-    x_column_index = header_map["missing_perc"];
-    missing_type_index = header_map["missing_type"]
-    evaluation_error_metric = 'MSE' //This needs to be interactive: RMSE, or MSE
-
-    for(line_index in lines){
+    plot_data = [];
+    for (line_index in lines) {
         line = lines[line_index]
         fields = line.split("\t")
 
-        var missing_type = fields[missing_type_index]
-        var desired_fields = {"x":fields[x_column_index]}
+        if (fields[missing_type_index] == missing_type) {
+            if (fields[evaluation_model_index] == evaluation_model) {
 
-        for (index in desired_columns_indexes){
-            column_index = desired_columns_indexes[index]
-            column_name = columns[column_index]
-            desired_fields[column_name] = fields[column_index]
+                var desired_fields = {"x":fields[x_variable_index]}
+
+                for (index in desired_columns_indexes){
+                    column_index = desired_columns_indexes[index]
+                    column_name = column_names[column_index]
+                    desired_fields[column_name] = fields[column_index]
+                }
+
+                plot_data.push(desired_fields)
+            }
         }
-
-        if (data[missing_type]) {
-            data[missing_type].push(desired_fields)
-        } else {
-            data[missing_type] = []
-            data[missing_type].push(desired_fields)
-        }  
     }
 
-    desired_missing_type = "MCAR" // This needs to be interactive
+    lines_data = [];
+    ranges_data = [];
 
+    if (evaluation_error_metric == 'RMSE') {
+
+        for (imputation_method_index in desired_columns) {
+            imputation_method = desired_columns[imputation_method_index]
+     
+            var one_line = plot_data.map(function(point, index) {
+            return [parseFloat(point["x"]), Math.sqrt(parseFloat(point[imputation_method]))];
+            });
+
+            lines_data.push(one_line)
+
+            if (confidence_interval) {
+
+                lower_bound_imputation_method = column_names[parseInt(header_map[imputation_method]) + parseInt(1)]
+                upper_bound_imputation_method = column_names[parseInt(header_map[imputation_method]) + parseInt(2)]
+
+                var one_range = plot_data.map(function(point, index) {
+                    return [parseFloat(point["x"]), Math.sqrt(parseFloat(point[lower_bound_imputation_method])), Math.sqrt(parseFloat(point[upper_bound_imputation_method]))];
+                });
+
+                ranges_data.push(one_range)
+                var y_axis_name = "RMSE"
+            }
+        }
+    } else {
+
+        for (imputation_method_index in desired_columns) {
+            imputation_method = desired_columns[imputation_method_index]
+     
+            var one_line = plot_data.map(function(point, index) {
+                return [parseFloat(point["x"]), parseFloat(point[imputation_method])];
+            });
+
+            lines_data.push(one_line)
+
+            if (confidence_interval) {
+
+                lower_bound_imputation_method = column_names[parseInt(header_map[imputation_method]) + parseInt(1)]
+                upper_bound_imputation_method = column_names[parseInt(header_map[imputation_method]) + parseInt(2)]
+
+                var one_range = plot_data.map(function(point, index) {
+                    return [parseFloat(point["x"]), parseFloat(point[lower_bound_imputation_method]), parseFloat(point[upper_bound_imputation_method])];
+                });
+
+                ranges_data.push(one_range)
+                var y_axis_name = "MSE"
+            }
+        }
+    }
+
+    // Set up plot
     series = [];    
-
     Highcharts.setOptions({
         colors: ['#b2182b', '#1b7837', '#2166ac']
     });
 
     for (imputation_method_index in desired_columns) {
+
         imputation_method = desired_columns[imputation_method_index]
-     
-        var line_data = data[desired_missing_type].map(function(point, index) {
-          return [parseFloat(point["x"]), parseFloat(point[imputation_method])];
-        });
+        line_data = lines_data[imputation_method_index]
 
         line = {
             name: imputation_method,
@@ -81,14 +133,9 @@ jQuery.get('data/results.txt', function(data) {
 
         series.push(line)
 
-        if (confidence_interval){
-            lower_bound_imputation_method = columns[parseInt(header_map[imputation_method]) + parseInt(1)]
-            upper_bound_imputation_method = columns[parseInt(header_map[imputation_method]) + parseInt(2)]
+        if (confidence_interval) {
 
-            var range_data = data[desired_missing_type].map(function(point, index) {
-                return [parseFloat(point["x"]), parseFloat(point[lower_bound_imputation_method]), parseFloat(point[upper_bound_imputation_method])];
-            });
-
+            range_data = ranges_data[imputation_method_index]
             var range = "ci range ";
 
             range = {
@@ -106,9 +153,12 @@ jQuery.get('data/results.txt', function(data) {
             }
 
             series.push(range)
+
         }
+
     }
 
+    // Create plot
     Highcharts.chart('container', {
 
     title: {
@@ -128,7 +178,7 @@ jQuery.get('data/results.txt', function(data) {
 
     yAxis: {
         title: {
-            text: 'MSE'
+            text: y_axis_name
         }
     },
 
